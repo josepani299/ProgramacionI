6ad2c7ad2a71dd03acffe54fe945c13db620d757
Jose Paniagua <Paniaguajose29u@gmail.com>
2024-10-25 10:28:23 -0300

    Modifique mi parte, poniendo estrucura

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Inicializacion de las variblaes opcion y salir. Nos permite seleccionar el modo y salir cuando el usuario diga.
+import random
 
 opcion = 0
 salir = 5
@@ -6,6 +7,7 @@ salir = 5
 # Elecciones para jugar
 
 while opcion != salir:
+    print("Bienvenido, elige una opcion: ")
     print("1 - Wordle")
     print("2 - Tablas")
     print("3 - Carreras dados")
@@ -20,8 +22,64 @@ while opcion != salir:
 
     def Wordle():
         print("Wordle")
+
+
+
+    # Parte de tablas
     def Tablas():
-        print("Tablas")
+
+        print("Bienvenido a tablas")
+        print("1- Repaso tablas")
+        print("2- Practicar tablas")
+        decisionInicial = int(input())
+        def repaso():
+            print("Las tablas son: ")
+            tablas = []
+            for k in range(10):
+                tablas.append([0]*10)
+
+            for i in range(10):
+                for j in range(10):
+                    tablas[i][j] = (i+1)*(j+1)
+
+            for i in range(10):
+                print("\n")
+                for j in range(10):
+                    print(tablas[i][j], end=" ")
+            print("\n")
+            Tablas()
+        def juego():
+            salir = False
+            while salir == False :
+                vidas = 3
+                racha = 0
+                while vidas > 0:
+                    numerouno = random.randint(1,10)
+                    numerodos = random.randint(1,10)
+                    print(f"Cuanto es {numerouno} * {numerodos}: ")
+                    respuesta = int(input())
+                    if respuesta == (numerodos * numerouno):
+                        racha += 1
+                    else:
+                        vidas -= 1
+                    print(f"Llevas una racha de {racha}")
+                    print(f"La cantidad de vida es: {vidas}")
+                decision = str(input("Quiere volver a jugar? Ingrese si o no"))
+                decision.lower()
+                if decision == "si":
+                    salir = False
+                elif decision == "no":
+                    salir = True
+                else:
+                    print("Error en respuesta. Solo se reciben  si o no como respuestas")
+
+        if decisionInicial == 1:
+            repaso()
+        elif decisionInicial == 2:
+            juego()
+        else:
+            print("No ingreso una opcion correcta")
+
     def Carreras():
         print("Carreras")
     def lenguaje():
